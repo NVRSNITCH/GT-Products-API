2668b1e7e028a22a0ed7baed171953a5bf34cff0
Joshua Jay F. Boncajes <defnot.chapo@gmail.com>
2025-09-26 19:28:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import postRoutes from "./src/routes/post.routes.js";
+import commentRoutes from "./src/routes/comment.routes.js";
+import userRoutes from "./src/routes/user.routes.js";
+import { testConnection } from "./src/config/db.js";
+import { errorHandler } from "./src/middlewares/errorHandler.middleware.js";
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-const PORT = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, Pinoy!");
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else {
+  null;
+}
+
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on http://localhost:${port}`
+  );
+  testConnection();
 });
-
-app.get("/info", (req, res) => {
-  res.status(300).send("Name: Marcus Neo Rangel, Section: IT4C, Program: IT");
-});
-
-app.get("/hello/:name", (req, res) => {
-  const name = req.params.name;
-  res.status(200).send("Hello " + name);
-});
-
-app.get("/foo", (req, res) => {
-  console.log(req.query);
-});
-
-/*
-app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  console.log(`Received request for ID: ${id}`);
-});
-*/
-
-app.get("/IT", (req, res) => {
-  const body = req.body;
-  console.log(body);
-});
-
-app.listen(PORT, () =>
-  console.log(`Server is running at http://localhost:${PORT}`)
-);
---
+++ src/services/user.service.js
@@ -0,0 +1,46 @@
+import pool from "../config/db.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const createUser = async (userData) => {
+  try {
+    const { username, email } = userData;
+    const [result] = await pool.query(
+      "INSERT INTO users (username, email, createdAt) VALUES (?, ?, NOW())",
+      [username, email]
+    );
+    if (result.affectedRows === 0) {
+      throw new ApiError(404, "User not found");
+    }
+
+    return { id: result.insertId, username, email };
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+      throw new ApiError(409, "User already exists");
+    }
+
+    throw error;
+  }
+};
+
+export const getUserById = async (id) => {
+  const [rows] = await pool.query("SELECT * FROM users WHERE id = ?", [id]);
+  if (rows.length === 0) {
+    throw new ApiError(404, "Not Found");
+  }
+  return rows[0];
+};
+
+export const getAllUsers = async () => {
+  const [users] = await pool.query("SELECT * FROM users");
+  return users;
+};
+
+export const getPostsByUser = async (userId) => {
+  const [posts] = await pool.query("SELECT * FROM posts WHERE authorId = ?", [
+    userId,
+  ]);
+  if (!posts) {
+    throw new ApiError(404, "No posts found for this user.");
+  }
+  return posts;
+};
